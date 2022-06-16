220e917320795777e3b87b1924c78511750df433
Oscar Miras <oscar.miras@gmail.com>
2022-06-16 10:12:15 +0200

    Create ex6.js

---
+++ ex6.js
@@ -0,0 +1,11 @@
+// Usa el método .filter para realizar esta kata: https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+
+// Ten en cuenta que la mayoría de métodos de array disponen de un segundo parámetro en la función de callback, que indica el índice sobre la posición en la se está iterando en ese momento
+
+function removeEveryOther(arr) {
+    //your code here
+}
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']), ['Hello', 'Hello Again']);
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [1, 3, 5, 7, 9]);
+console.log(removeEveryOther([['Goodbye'], { 'Great': 'Job' }]), [['Goodbye']]);
