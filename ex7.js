51ccc4a6be8e4ac1f30133f0a6ae7217c598058c
Oscar Miras <oscar.miras@gmail.com>
2023-05-28 07:13:08 +0000

    ex7

--- ex7.js
+++ ex7.js
@@ -1,6 +1,23 @@
-/** Solo modifica el cuerpo de la función. Implementa esta función. */
+/** Solo modifica el cuerpo de la función. Implementa una función que dado un objeto que representa una persona, devuelva ùn objeto nuevo, con las mismas propiedades, pero además, con la propiedad 'edad' */
+/** Prueba de implementar dos versiones: 1. crando un objeto de 0 con las propiedades que se piden. 2. usa el opreador de rest (...) par copiar las propiedades del objeto 'persona' y añade la propiedad 'edad'  */
 function calcularEdad(persona) {
+
+    const currentYear = new Date().currentDate.getFullYear();
+    let newPerson = {};
+
+    // Modificar a partir de aqui
+
+    // NO modificar a partir de aquí
+    return newPerson;
   
 }
 
-console.log(
+console.log(calcularEdad({
+    nombre: 'Marcos',
+    nacimiento: '1999'
+})); // { nombre: 'Marcos', nacimiento: '1999', edad: 24}
+
+console.log(calcularEdad({
+    nombre: 'Maria',
+    nacimiento: '2003'
+})); // { nombre: 'Maria', nacimiento: '2003', edad: 20}
