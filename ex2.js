416dbe230787ef500f3ec276775dee3cc529ece8
Oscar Miras <oscar.miras@gmail.com>
2024-06-16 09:51:55 +0200

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -1,15 +1,16 @@
 
 // Uso adecuado de let y scope para actualizar una variable
+// Somos portero de una discoteca y nos indican la edad de la persona que quiere entrar
 
 // https://www.w3schools.com/react/react_es6_variables.asp
 
-let puedesPasar = true;
-edad = 17;
+let puedesPasar = false;
+let edad = 21;
 
-if (edad < 18) {
-    let puedesPasar = false;
+if (edad >= 18) {
+    let puedesPasar = true;
 }
 
-console.log(puedesPasar) // false
+console.log(puedesPasar) // Debería ser true pero vale false. ¿por qué?
 
 // PREGUNTA: ¿Por qué no se actualiza correctamente la variable 'puedesPasar'?
